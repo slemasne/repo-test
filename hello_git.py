bf4b7bb5ea701bc1b6254542400ba826e31f2e5b
Stephen LeMasney <stephen.lemasney@finbourne.com>
2020-05-24 13:34:04 +0100

    test

--- hello_git.py
+++ hello_git.py
@@ -5,3 +5,11 @@ test_string = f"Hello from slemasne at {now}"
 
 print(test_string)
 print("Now making another change")
+
+def test_func(a):
+
+	test =        'Test function: '
+
+	return test + a 
+
+print( test_func("stephen"))
