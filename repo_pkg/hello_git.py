2592b26a23b77d06ecba7653727fee2fdecc4c3e
Stephen LeMasney <stephen.lemasney@finbourne.com>
2020-05-25 12:16:24 +0100

    test

---
+++ repo_pkg/hello_git.py
@@ -0,0 +1,16 @@
+import datetime
+
+now = datetime.datetime.now()
+test_string = f"Hello from slemasne at {now}"
+
+print(test_string)
+print("Now making another change")
+
+
+test = 'test test'
+
+
+test_dict = {"test1":
+2}
+
+test = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiggggggggggggg___strrrrrrrrrrrrrrrrring_testttttttttttttttttttttttttttttttttttttttttttttttttt"
