0ddee0a778d50e4babb71db23ea6d617c3ff3eee
GitHub Action <action@github.com>
2020-05-28 16:22:38 +0100

    test1

--- new_commit_with_black.sh
+++ new_commit_with_black.sh
@@ -3,7 +3,7 @@
 if [ -n "$(git status --porcelain)" ]; then 
 git config --local user.email "action@github.com" 
 git config --local user.name "GitHub Action" 
-git commit -m "black code formatting [ci skip]" -a 
+git commit -m "black code formatting" -a 
 git push https://slemasne:${GH_TOKEN}@github.com/slemasne/repo-test.git HEAD:$TRAVIS_BRANCH
 echo "The black formating tool has made changes. Making commit."
 else 
