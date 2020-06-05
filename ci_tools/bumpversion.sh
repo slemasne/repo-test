e8e9aada93ab1144efa75bb90609e632013aa0a6
GitHub Action <action@github.com>
2020-06-05 13:05:35 +0100

    test

--- ci_tools/bumpversion.sh
+++ ci_tools/bumpversion.sh
@@ -1,5 +1,5 @@
 bump2version patch --message "Bump version: {current_version} to {new_version} [ci skip]"
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
-git push https://slemasne:${GH_TOKEN}@github.com/slemasne/repo-test.git HEAD:$TRAVIS_BRANCH
+git push https://slemasne:${GH_TOKEN}@github.com/slemasne/repo-test.git master
 echo "Making commit with incremented version"
--- ci_tools/new_commit_with_black.sh
+++ ci_tools/new_commit_with_black.sh
@@ -3,7 +3,7 @@ if [ -n "$(git status --porcelain)" ]; then
 git config --local user.email "action@github.com" 
 git config --local user.name "GitHub Action" 
 git commit -m "black code formatting" -a 
-git push https://slemasne:${GH_TOKEN}@github.com/slemasne/repo-test.git HEAD:$TRAVIS_BRANCH
+git push https://slemasne:${GH_TOKEN}@github.com/slemasne/repo-test.git $TRAVIS_BRANCH
 echo "The black formating tool has made changes. Making commit."
 else 
 echo "No changes detected. Skipping commit"; 
