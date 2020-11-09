2d5a096245cafcea72eac47a0265ce0a494dc9c7
Stephen LM <20342969+slemasne@users.noreply.github.com>
2020-11-09 11:45:43 +0000

    Update bumpversion.sh

--- ci_tools/bumpversion.sh
+++ ci_tools/bumpversion.sh
@@ -2,21 +2,26 @@
 
 last_commit_message=$(git log -1 HEAD --pretty=format:%s)
 
-# find next version
-api_version=$(cat __version__.py | grep __version__ |  awk '{split($0, a, "="); print a[2]}' | tr -d ' "')
-IFS='.' read -a version_split <<< "${api_version}"
-current_patch=${version_split[2]}
-new_patch=$(echo "$current_patch + 1" | tr -d $'\r' | bc)
-new_version="${version_split[0]}.${version_split[1]}.$new_patch"
+# Bump the version up
 
-echo "Last commit message: $last_commit_message"
 if [[ "$last_commit_message" == "Bump version"* ]]; then
-	echo "last commit is a bump version commit... skipping..."
+        echo "Skipping version increment (no commits found)"
 else
-	echo "Bumping packaging patch version from $api_version to $new_version"
-	bump2version patch --message "Bump version in Travis CI: {current_version} to {new_version} [ci skip]"
-	git config --global user.email "slemasne@test.com"
-	git config --global user.name "slm-travis-bot"
-	git push https://slemasne:${GH_TOKEN}@github.com/slemasne/repo-test.git HEAD:$TRAVIS_BRANCH
-	echo "Making commit with incremented version"
+
+	# Find next version
+
+	api_version=$(cat __version__.py | grep __version__ |  awk '{split($0, a, "="); print a[2]}' | tr -d ' "')
+	IFS='.' read -a version_split <<< "${api_version}"
+	current_patch=${version_split[2]}
+	new_patch=$(echo "$current_patch + 1" | tr -d $'\r' | bc)
+	new_version="${version_split[0]}.${version_split[1]}.$new_patch"
+    echo "Bumping packaging patch version from $api_version to $new_version"
+
+    # Bump the version in project and make commit back to master
+
+    bump2version patch --message "Bump version in in GitHub Actions: {current_version} to {new_version} [ci skip]"
+	git config --local user.email "stephen@testing.com"
+	git config --local user.name "slemasne"
+	git push https://slemasne:${GH_TOKEN}@github.com/slemasne/repo-test.git HEAD:master
+	echo "Commit with incremented version completed"
 fi
